3194c97c01eb618aad39b0fc1193771e4909495b
Psy-Fer <fergo1988@gmail.com>
2026-05-29 18:01:31 +1000

    add missing n_genome_real for #51

--- src/junction/mod.rs
+++ src/junction/mod.rs
@@ -409,6 +409,7 @@ mod tests {
         let genome = Genome {
             sequence: vec![0; 4000],
             n_genome: 2000,
+            n_genome_real: 2000,
             n_chr_real: 2,
             chr_start: vec![0, 1000, 2000],
             chr_length: vec![1000, 1000],
